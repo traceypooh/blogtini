308823618b4c4caa2654aed195033484ee4694a1
Tracey Jaquith <tracey@archive.org>
2022-08-07 13:37:30 -0400

    whitespace fixes

--- js/future-imperfect.js
+++ js/future-imperfect.js
@@ -17,42 +17,42 @@ var $searchResults;     // The element on the page holding search results
 var $searchInput;       // The search box element
 
 function search_setup(docs) {
-$.each(toggles, function(toggle, menu) {
-  $(toggle).on("click", function() {
-    if ($(menu).hasClass("active")) {
+  $.each(toggles, function(toggle, menu) {
+    $(toggle).on("click", function() {
+      if ($(menu).hasClass("active")) {
+        $(".menu").removeClass("active");
+        $("#wrapper").removeClass("overlay");
+      } else {
+        $("#wrapper").addClass("overlay");
+        $(".menu").not($(menu + ".menu")).removeClass("active");
+        $(menu).addClass("active");
+        if (menu == "#search-input") {$("#search-results").toggleClass("active");}
+      }
+    });
+  });
+
+  // Click anywhere outside a flyout to close
+  $(document).on("click", function(e) {
+    if ($(e.target).is(".lang-toggle, .lang-toggle span, #lang-menu, .share-toggle, .share-toggle i, #share-menu, .search-toggle, .search-toggle i, #search-input, #search-results .mini-post, .nav-toggle, .nav-toggle i, #site-nav") === false) {
       $(".menu").removeClass("active");
-      $("#wrapper").removeClass("overlay");
-    } else {
-      $("#wrapper").addClass("overlay");
-      $(".menu").not($(menu + ".menu")).removeClass("active");
-      $(menu).addClass("active");
-      if (menu == "#search-input") {$("#search-results").toggleClass("active");}
+      $("#wrapper").removeClass('overlay');
     }
   });
-});
 
-// Click anywhere outside a flyout to close
-$(document).on("click", function(e) {
-  if ($(e.target).is(".lang-toggle, .lang-toggle span, #lang-menu, .share-toggle, .share-toggle i, #share-menu, .search-toggle, .search-toggle i, #search-input, #search-results .mini-post, .nav-toggle, .nav-toggle i, #site-nav") === false) {
-    $(".menu").removeClass("active");
-    $("#wrapper").removeClass('overlay');
-  }
-});
+  // Check to see if the window is top if not then display button
+  $(window).scroll(function() {
+    if ($(this).scrollTop()) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
 
-// Check to see if the window is top if not then display button
-$(window).scroll(function() {
-  if ($(this).scrollTop()) {
-    $('#back-to-top').fadeIn();
-  } else {
-    $('#back-to-top').fadeOut();
-  }
-});
-
-// Click event to scroll to top
-$('#back-to-top').click(function() {
-  $('html, body').animate({scrollTop: 0}, 1000);
-  return false;
-});
+  // Click event to scroll to top
+  $('#back-to-top').click(function() {
+    $('html, body').animate({scrollTop: 0}, 1000);
+    return false;
+  });
 
 
   // Get dom objects for the elements we'll be interacting with
